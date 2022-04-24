168574ae0ad311154999973adf79b958d04eb457
Pierre Souza <pas@lionx.com.br>
2022-04-24 14:34:04 -0300

    study

---
+++ modulo7.js/oo.js
@@ -0,0 +1,22 @@
+// CODIGO NÃO EXECUTÁVEL
+
+//procedural
+processamento(valor1,valor2,valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()
+
+// Principais importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
