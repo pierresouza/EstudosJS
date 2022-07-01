3d79fed0b23725fc1ca0cc711ca3017ad373934c
Pierre Souza <pas@lionx.com.br>
2022-07-01 11:57:42 -0300

    update

---
+++ funcoesArraysRocket/desafios/desafio06.js
@@ -0,0 +1,8 @@
+/*
+  6. Crie um array que tenha em suas posições de A - Z.
+
+  - Dicas:
+    - Cada caracter de uma  `String` corresponde a um número. use `String.charCodeAt(0)`(Sendo 0 a posição inicial da cade de String) para pegar o número de referência da String desejada
+    -É possível transformar um número em uma String. Utilize `String.fromCharCode(<x>)` Sendo x o número de referência do caracter.
+    - Utilize a função anterior para criar um intervalo número correspondente de A
+*/
